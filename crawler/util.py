1e6ff6eaa70a9b8c13b854d2fceb550cbaca6314
Chen Jianhua <981374642@qq.com>
2019-03-26 22:17:22 +0800

    去掉小标签【1】

--- crawler/util.py
+++ crawler/util.py
@@ -3,6 +3,6 @@ import requests
 
 def get(url):
     try:
-        return requests.get(url)
+        return requests.get(url, headers={'user-agent': 'my-app/0.0.1'})
     except Exception as e:
         print(e, 'from', url)
