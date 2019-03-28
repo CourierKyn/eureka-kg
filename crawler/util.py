87f64608d0a798cddfeaef843e7eac9a4eff179f
Courier Kyn <franklin_a_h_p@qq.com>
2019-03-28 20:42:13 +0800

    改进 crawler/util.py

--- crawler/util.py
+++ crawler/util.py
@@ -2,7 +2,10 @@ import requests
 
 
 def get(url):
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6',
+    }
     try:
-        return requests.get(url, headers={'user-agent': 'my-app/0.0.1'})
+        return requests.get(url, headers=headers)
     except Exception as e:
         print(e, 'from', url)
