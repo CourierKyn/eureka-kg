6bc5099898aee56798b517a017987faa4f7111ac
Jason Xu <1030015581@qq.com>
2019-04-01 22:39:46 +0800

    elasticsearch测试完成

--- server/app/es_test.py
+++ server/app/es_test.py
@@ -3,15 +3,30 @@
 # conn.create_index('test-index')#新建一个索引
 
 from elasticsearch import Elasticsearch
-
+import json
+import re
+data = [json.loads(line) for line in open('data/iotworm.jsonlines')]
 es = Elasticsearch()
 es.indices.create(index='news', ignore=400)
 #删除数据
 # result = es.indices.delete(index='news', ignore=[400, 404])
 #更新数据
-# data = {'title': '美国留给伊拉克的是个烂摊子吗', 'url': 'http://view.news.qq.com/zt2011/usa_iraq/index.htm', 'date': '2011-12-16'}
+# data = {'title': 'for update', 'url': 'http://view.news.qq.com/zt2011/usa_iraq/index.htm', 'date': '2011-12-16'}
 # result = es.update(index='news', doc_type='politics', body=data, id=1)
 #添加数据
-data = {'title': '美国留给伊拉克的是个烂摊子吗', 'url': 'http://view.news.qq.com/zt2011/usa_iraq/index.htm'}
-result = es.create(index='news', doc_type='politics', id=1, body=data)
-print(result)
+# i = 1
+# for d in data:
+#     result = es.create(index='news', doc_type='politics', id=i, body=d)
+#     i+=1
+#     print(result)
+#查询
+dsl = {
+    'query': {
+        'match': {
+            'summary': 'iot fintech'
+        }
+    }
+}
+result = es.search(index='news', doc_type='politics',body=dsl)
+print(json.dumps(result, sort_keys=True, indent=4))#json格式化输出
+
